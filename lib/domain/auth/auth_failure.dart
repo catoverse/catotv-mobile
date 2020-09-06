a60b078cf77d81ea8b0f3aa2d25c9228600eef95
Ravin <ravindrabarthwal.99@gmail.com>
2020-09-06 15:14:08 +0530

    Added Auth Facade

---
+++ lib/domain/auth/auth_failure.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'auth_failure.freezed.dart';
+
+@freezed
+abstract class AuthFailure with _$AuthFailure {
+  const factory AuthFailure.cancelledByUser() = _CancelledByUser;
+  const factory AuthFailure.serverError() = _ServerError;
+}
