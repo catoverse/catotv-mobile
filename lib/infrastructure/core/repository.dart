e3d5b0058c68bf546c859eae7218f35ba4ed9eb2
Ravin <ravindrabarthwal.99@gmail.com>
2020-09-06 20:12:55 +0530

    FEAT: Repository Implementation

---
+++ lib/infrastructure/core/repository.dart
@@ -0,0 +1,32 @@
+import 'package:cato_feed/domain/core/failure.dart';
+import 'package:cato_feed/domain/core/i_logger.dart';
+import 'package:cato_feed/domain/core/i_repository.dart';
+import 'package:cato_feed/infrastructure/core/remote/network.dart';
+import 'package:get_version/get_version.dart';
+import 'package:injectable/injectable.dart';
+import 'package:cato_feed/domain/core/result.dart';
+
+
+
+@LazySingleton(as: IRepository)
+class Repository implements IRepository {
+  final Network _network;
+  final ILogger _logger;
+
+  Repository(this._network, this._logger);
+
+  /// Returns whether force update is required or not.
+  @override
+  Future<Result<Failure, bool>> isForceUpdateRequired() async{
+    var result = await _network.getMinimumVersion();
+    if(result.hasFailed()) return Result.fail(result.failure);
+
+    try {
+      var currentVersion = int.parse(await GetVersion.projectCode);
+      return Result.data(result.data > currentVersion);
+    } catch (err){
+      _logger.logException(err);
+      return Result.fail(Failure.message('Error while checking for the app update.'));
+    }
+  }
+}
