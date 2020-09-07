3f7ee4cc63b1289cd675df9724dc1edf21202bb7
Ravin <ravindrabarthwal.99@gmail.com>
2020-09-07 18:59:34 +0530

    FEAT: Added Main Feed

---
+++ lib/presentation/screens/home/main_feed.dart
@@ -0,0 +1,213 @@
+import 'dart:ui';
+
+import 'package:cato_feed/application/home/feed/feed_bloc.dart';
+import 'package:cato_feed/application/post/post.dart';
+import 'package:cato_feed/application/topic/topic.dart';
+import 'package:cato_feed/domain/posts/post.dart';
+import 'package:cato_feed/injection.dart';
+import 'package:cato_feed/presentation/utils/assets/color_assets.dart';
+import 'package:cato_feed/presentation/widgets/horizontal_tags.dart';
+import 'package:cato_feed/presentation/widgets/post_widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+class MainFeedScreen extends StatelessWidget {
+  Key _key;
+
+  MainFeedScreen({Key key}) : super(key: key) {
+    _key = key;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => getIt<FeedBloc>(),
+      child: MainFeedPage(
+        key: _key,
+      ),
+    );
+  }
+}
+
+class MainFeedPage extends StatefulWidget {
+  MainFeedPage({Key key}) : super(key: key);
+
+  @override
+  _MainFeedPageState createState() => _MainFeedPageState();
+}
+
+class _MainFeedPageState extends State<MainFeedPage> {
+  PostDataSource _dataSource;
+
+  @override
+  void initState() {
+    _dataSource = PostDataSource(0, 15, context.bloc<FeedBloc>());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _dataSource.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<PostBloc, PostState>(
+          listener: (_, state) {
+            // ignore: close_sinks
+            var feedBloc = context.bloc<FeedBloc>();
+            if (feedBloc.state.selectedTopicId == null) {
+              // Load All
+              if (state.allFeed.posts != null) {
+                _dataSource.notifyChange(
+                    state.allFeed.posts, null, state.allFeed.skip);
+              }
+              if (state.allFeed.posts != null || state.failure != null) {
+                feedBloc.add(FeedEvent.loadFinished());
+              }
+            } else {
+              // Load from topic
+              if (state.feedByTopic.posts != null) {
+                _dataSource.notifyChange(
+                    state.feedByTopic.posts, null, state.feedByTopic.skip);
+              }
+              if (state.feedByTopic.posts != null || state.failure != null) {
+                feedBloc.add(FeedEvent.loadFinished());
+              }
+            }
+          },
+        ),
+        BlocListener<FeedBloc, FeedState>(
+          listener: (_, state) {
+            if (state.isLoading == false) {
+              var event = (state.selectedTopicId == null)
+                  ? PostEvent.loadFeed(state.nextPageKey, state.pageSize)
+                  : PostEvent.loadFeedByTopic(
+                      state.nextPageKey, state.pageSize, state.selectedTopicId);
+              context.bloc<PostBloc>().add(event);
+            }
+          },
+        ),
+      ],
+      child: PlatformScaffold(
+        backgroundColor: ColorAssets.black32,
+        appBar: PlatformAppBar(
+          backgroundColor: ColorAssets.black21,
+          material: (_, __) {
+            return MaterialAppBarData(
+              flexibleSpace: BlocBuilder<TopicBloc, TopicState>(
+                builder: (_, state) {
+                  var topics = state.allTopics
+                      .where((element) =>
+                          state.selectedTopicIds.contains(element.id))
+                      .toList();
+                  return Container(
+                    height: 32,
+                    margin: EdgeInsets.only(top: 35, bottom: 10),
+                    child: BlocBuilder<FeedBloc, FeedState>(
+                      builder: (_, state) {
+                        return HorizontalTags(
+                            topics,
+                            state.selectedTopicId,
+                            onTap: (selectedTopicId) {
+                              if(state.selectedTopicId != selectedTopicId) {
+                                context.bloc<FeedBloc>().add(FeedEvent.selectTopic(topicId: selectedTopicId));
+                              }
+                            }
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+        body: PagedListView.separated(
+          addRepaintBoundaries: false,
+          padding: EdgeInsets.only(top: 20),
+          separatorBuilder: (_, __) {
+            return SizedBox(
+              height: 20,
+            );
+          },
+          key: PageStorageKey('feed'),
+          dataSource: _dataSource,
+          builderDelegate: PagedChildBuilderDelegate<Post>(
+            itemBuilder: (context, post, index) {
+              return PostWidget(key: ValueKey(post.id), post: post);
+            },
+            newPageProgressIndicatorBuilder: (context) {
+              return Container(
+                margin: EdgeInsets.only(top: 8, bottom: 8),
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            },
+            firstPageProgressIndicatorBuilder: (context) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PostDataSource extends PagedDataSource<int, Post> {
+  final int pageSize;
+  final FeedBloc bloc;
+
+  PostDataSource(int firstPageKey, this.pageSize, this.bloc)
+      : super(firstPageKey);
+
+  @override
+  void fetchItems(int pageKey) {
+    bloc.add(FeedEvent.load(nextPageKey, pageSize));
+  }
+}
+
+/**
+
+    Column(
+    key: PageStorageKey('feed'),
+    children: [
+    Card(
+    margin: EdgeInsets.only(bottom: 1),
+    elevation: 1,
+    child: Container(
+    height: 32,
+    margin: EdgeInsets.only(top: 45, bottom: 10),
+    child: HorizontalTags(),
+    ),
+    ),
+    Expanded(
+    child: MediaQuery.removePadding(
+    context: context,
+    child: ListView.separated(
+    separatorBuilder: (_, index) {
+    return SizedBox(
+    height: 30,
+    );
+    },
+    itemCount: 10,
+    itemBuilder: (_, index) {
+    return PostWidget();
+    },
+    ),
+    removeTop: true,
+    ),
+    ),
+    ],
+    )
+
+ **/
---
+++ lib/presentation/widgets/post_widgets.dart
@@ -0,0 +1,262 @@
+import 'package:cato_feed/domain/posts/post.dart';
+import 'package:cato_feed/presentation/utils/assets/color_assets.dart';
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class _PostTitleWidget extends StatelessWidget {
+  final String title;
+
+  _PostTitleWidget({@required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 15,
+        ),
+        Expanded(
+          flex: 1,
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+            maxLines: 1,
+            textWidthBasis: TextWidthBasis.parent,
+          ),
+        ),
+        SizedBox(
+          width: 15,
+        )
+      ],
+    );
+  }
+}
+
+class _PostMediaWidget extends StatefulWidget {
+  final String youtubeVideoId;
+  final int startTime;
+  final int endTime;
+
+  const _PostMediaWidget({Key key, this.youtubeVideoId, this.startTime, this.endTime})
+      : super(key: key);
+
+  @override
+  _PostMediaWidgetState createState() =>
+      _PostMediaWidgetState(youtubeVideoId, startTime, endTime);
+}
+
+class _PostMediaWidgetState extends State<_PostMediaWidget> {
+  final String youtubeVideoId;
+  final int startTime;
+  final int endTime;
+
+  YoutubePlayerController _controller;
+  PlayerState _playerState;
+  YoutubeMetaData _youtubeMetaData;
+  bool _isPlayerReady = false;
+
+  _PostMediaWidgetState(this.youtubeVideoId, this.startTime, this.endTime);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: youtubeVideoId,
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+        enableCaption: false,
+        controlsVisibleAtStart: false,
+        hideControls: false,
+        loop: false,
+        disableDragSeek: false,
+        startAt: startTime,
+        endAt: endTime,
+      ),
+    )..addListener(_youtubePlayerListener);
+    _youtubeMetaData = YoutubeMetaData();
+    _playerState = PlayerState.unknown;
+  }
+
+  void _youtubePlayerListener() {
+    if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
+      setState(() {
+        _playerState = _controller.value.playerState;
+        _youtubeMetaData = _controller.metadata;
+      });
+    }
+  }
+
+  @override
+  void deactivate() {
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.removeListener(_youtubePlayerListener);
+    _controller.dispose();
+    _controller = null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // var youtubeId = YoutubePlayer.convertUrlToId(videoUrl);
+    // return AnimatedOpacity(
+    //   opacity: 1,
+    //   duration: const Duration(milliseconds: 300),
+    //   child: Image.network(
+    //     YoutubePlayer.getThumbnail(videoId: youtubeId),
+    //   ),
+    // );
+    return YoutubePlayer(
+      controller: _controller,
+      onReady: () {
+        _isPlayerReady = true;
+      },
+      bottomActions: [],
+      showVideoProgressIndicator: true,
+    );
+  }
+}
+
+class _PostSocialStatsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 15,
+        ),
+        Text('6M views'),
+        SizedBox(
+          width: 5,
+        ),
+        Text('|'),
+        SizedBox(
+          width: 5,
+        ),
+        Text('Liked by rutvijkarkhanis and 20k others')
+      ],
+    );
+  }
+}
+
+class _PostSocialInteractionWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 15,
+        ),
+        Icon(
+          Icons.favorite_border,
+          color: Colors.white,
+          size: 30,
+        ),
+        SizedBox(
+          width: 24,
+        ),
+        Icon(
+          Icons.bookmark_border,
+          size: 30,
+          color: Colors.white,
+        ),
+        Spacer(),
+        Icon(
+          Icons.share,
+          size: 30,
+          color: Colors.white,
+        ),
+        SizedBox(
+          width: 15,
+        )
+      ],
+    );
+  }
+}
+
+class _PostDescription extends StatelessWidget {
+  final String authorName;
+  final String description;
+
+  const _PostDescription({this.authorName, this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    var isTextEmpty = authorName.isEmpty && description.isEmpty;
+    if (isTextEmpty) return Container();
+    return Container(
+      margin: EdgeInsets.only(left: 15, right: 15, top: 8.0, bottom: 8.0),
+      child: RichText(
+        text: TextSpan(
+          style: TextStyle(color: Colors.white),
+          children: <TextSpan>[
+            TextSpan(
+                text: authorName,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            TextSpan(text: ' '),
+            TextSpan(text: description)
+          ],
+        ),
+        overflow: TextOverflow.ellipsis,
+        maxLines: 2,
+      ),
+    );
+  }
+}
+
+class PostWidget extends StatelessWidget {
+  final Post post;
+
+  const PostWidget({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var youtubeVideoId = YoutubePlayer.convertUrlToId(post.videoUrl);
+    if(youtubeVideoId == null) return Container();
+    return Card(
+      color: ColorAssets.black21,
+      margin: EdgeInsets.only(left: 15, right: 15),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 12,
+          ),
+          _PostTitleWidget(
+            title: post.title ?? '',
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          _PostMediaWidget(
+            youtubeVideoId: youtubeVideoId,
+            startTime: post.startTimestamp,
+            endTime: post.endTimestamp,
+          ),
+          // SizedBox(
+          //   height: 5,
+          // ),
+          // _PostSocialStatsWidget(),
+          SizedBox(
+            height: 10,
+          ),
+          _PostSocialInteractionWidget(),
+          SizedBox(
+            height: 10,
+          ),
+          _PostDescription(
+            authorName: post.authorName,
+            description: post.description,
+          ),
+        ],
+      ),
+    );
+  }
+}
