140421bd2aa0ac06e99d67c5623fdc0e00b7b827
Ravin <ravindrabarthwal.99@gmail.com>
2020-09-10 11:06:22 +0530

    FEAT: Updated Android Configuration for AppCenter

---
+++ android/app/appcenter-post-clone.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# place this script in project/android/app/
+cd ..
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+cd ..
+# choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
+# stable - recommended for production
+
+git clone -b stable https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter channel stable
+flutter doctor
+flutter build apk --release
+flutter build appbundle --release
+
+# copy the APK where AppCenter will find it
+#mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_<
