8b9f3f7e108a64f629fb12c01378350a01532a33
Ravindra Barthwal <ravindrabarthwal.99@gmail.com>
2021-02-03 03:35:21 +0530

    Poppins Font Added

--- lib/presentation/utils/assets/font_assets.dart
+++ lib/presentation/utils/assets/font_assets.dart
@@ -3,4 +3,5 @@ class FontAssets {
   // Declare all custom fonts here
   static final String Roboto = 'Roboto';
   static final String SFProDisplay = 'SFProDisplay';
+  static final String Poppins = 'Poppins';
 }
