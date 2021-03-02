e9154918957f44ee2816c0e48a701aa13721c468
Ravindra Barthwal <ravindrabarthwal.99@gmail.com>
2021-03-02 12:15:10 +0530

    Search Result Page
    Search Result page with empty and a list of search results videos

---
+++ lib/presentation/screens/search/search_result.dart
@@ -0,0 +1,74 @@
+import 'package:cato_feed/presentation/utils/assets/font_assets.dart';
+import 'package:flutter/material.dart';
+
+import 'horizontal_video_tile.dart';
+
+class SearchResult extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+        child: _buildEmptySearchResult());
+  }
+
+  Widget _buildSearchResult() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '12 Results found',
+          maxLines: 2,
+          softWrap: true,
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontFamily: FontAssets.Poppins,
+            fontSize: 12,
+            color: Color(0xFF0C3C57),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return HorizontalVideoTile();
+            },
+            itemCount: 10,
+            separatorBuilder: (context, index) {
+              return SizedBox(
+                height: 16,
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEmptySearchResult() {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            Icons.search_rounded,
+            color: Color(0xFFC4C4C4),
+            size: 160,
+          ),
+          Text(
+            'No Search Found',
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontFamily: FontAssets.Poppins,
+              fontSize: 12,
+              color: Color(0xFF8F8F8F),
+            ),
+          ),
+          SizedBox(height: 96,)
+        ],
+      ),
+    );
+  }
+}
