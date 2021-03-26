0d8992465210ced4673d73c8d2cfa30eb7cf756b
Ravindra Barthwal <ravindrabarthwal.99@gmail.com>
2021-03-26 19:00:08 +0530

    SearchScreen

--- lib/presentation/screens/search/search.dart
+++ lib/presentation/screens/search/search.dart
@@ -10,7 +10,7 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SearchPage();
-  } 
+  }
 }
 
 class SearchPage extends StatelessWidget {
