15ea7ebdfa5148d95440ce0de5ecfbaae77e55dc
algokun <themightyking117@gmail.com>
2021-05-17 22:30:05 +0530

    ✨ improved explore ui

--- lib/ui/explore/explore_view.dart
+++ lib/ui/explore/explore_view.dart
@@ -1,4 +1,7 @@
+import 'package:feed/ui/custom/input_field.dart';
 import 'package:feed/ui/global/screen.dart';
+import 'package:feed/ui/global/styles.dart';
+import 'package:feed/ui/global/theme.dart';
 import 'package:flutter/material.dart';
 
 import 'explore_viewmodel.dart';
@@ -9,11 +12,42 @@ class ExploreView extends StatelessWidget {
     return ScreenBuilder<ExploreViewModel>(
         viewModel: ExploreViewModel(),
         builder: (context, uiHelpers, model) => ListView(
-              children: model.topics
-                  .map((e) => ListTile(
-                        title: Text(e.name),
-                      ))
-                  .toList(),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+              children: [
+                Text(
+                  "Search",
+                  style: uiHelpers.heading,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                  decoration: textFieldStyle,
+                  child: Text(
+                    "What would you like to know? ",
+                    style: uiHelpers.button,
+                  ),
+                ),
+                Wrap(
+                  spacing: 5,
+                  runSpacing: 5,
+                  children: model.topics
+                      .map((e) => Card(
+                            child: Container(
+                              width: uiHelpers.blockSizeHorizontal! * 45,
+                              height: uiHelpers.blockSizeVertical! * 15,
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                e.name,
+                                style: uiHelpers.subheading,
+                              ),
+                            ),
+                            elevation: 2.0,
+                            color: AppColors.surface,
+                          ))
+                      .toList(),
+                ),
+              ],
             ));
   }
 }
