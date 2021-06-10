9543a7e9ec2f03a3aa754ff68037dfc04287cfe2
algokun <themightyking117@gmail.com>
2021-06-10 23:47:16 +0530

    ✨ updated back-end api

--- lib/ui/onboarding/onboarding_viewmodel.dart
+++ lib/ui/onboarding/onboarding_viewmodel.dart
@@ -26,35 +26,3 @@ class OnboardingViewModel extends ReactiveViewModel with SnackbarHelper {
   @override
   List<ReactiveServiceMixin> get reactiveServices => [_feedImpl];
 }
-
-// class OnboardingViewModel extends BaseViewModel with SnackbarHelper {
-//   int currentIndex = 0;
-//   final PageController controller = PageController(initialPage: 0);
-//   final NavigationService _navigationService = locator<NavigationService>();
-
-//   void onPageChanged(int page) {
-//     currentIndex = page;
-
-//     notifyListeners();
-//   }
-
-//   void makePagesLoop() {
-//     Timer.periodic(Duration(seconds: 4), (Timer timer) {
-//       if (currentIndex < 2) {
-//         currentIndex++;
-//       } else {
-//         currentIndex = 0;
-//       }
-
-//       controller.animateToPage(
-//         currentIndex,
-//         duration: Duration(milliseconds: 250),
-//         curve: Curves.easeIn,
-//       );
-//     });
-//   }
-
-//   Future navigateToRestrictedHome() async {
-//     _navigationService.navigateTo(Routes.restrictedHomeView);
-//   }
-// }
--- lib/ui/startup/startup_viewmodel.dart
+++ lib/ui/startup/startup_viewmodel.dart
@@ -2,26 +2,27 @@ import 'package:feed/app/app.logger.dart';
 import 'package:feed/app/app.locator.dart';
 import 'package:feed/app/app.router.dart';
 import 'package:feed/core/services/user_service/user_service.dart';
+import 'package:feed/remote/api/api_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class StartUpViewModel extends BaseViewModel {
   final _log = getLogger('StartUpViewModel');
   final UserService _userService = locator<UserService>();
-  // final APIService _apiService = locator<APIService>();
+  final APIService _apiService = locator<APIService>();
 
   final NavigationService _navigationService = locator<NavigationService>();
 
   Future<void> runStartupLogic() async {
     bool isLoggedIn = _userService.hasLoggedInUser();
-    // var forceUpdateRequired = await _apiService.checkUpdateRequired();
+    var forceUpdateRequired = await _apiService.checkUpdateRequired();
 
-    // _log.v(forceUpdateRequired);
+    _log.v(forceUpdateRequired);
 
-    // if (forceUpdateRequired is bool && forceUpdateRequired) {
-    //   _log.v('The app needs to be updated, So redirect to update app screen');
-    //   return _navigationService.navigateTo(Routes.updateView);
-    // }
+    if (forceUpdateRequired is bool && forceUpdateRequired) {
+      _log.v('The app needs to be updated, So redirect to update app screen');
+      return _navigationService.navigateTo(Routes.updateView);
+    }
 
     if (isLoggedIn) {
       _log.v('We have a user session on disk. Sync the user profile ...');
